c6964aaafabf6dea3b537b5eea41f3fc4db0b753
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-25 14:38:54 -0300

    feat: add model Automobile

--- src/models/Automobile.ts
+++ src/models/Automobile.ts
@@ -2,4 +2,19 @@ import { v4 as uuid } from "uuid";
 
 class Automobile {
   id: string;
+
+  placa: string;
+
+  cor: string;
+
+  marca: string;
+
+  constructor(placa: string, cor: string, marca: string) {
+    this.id = uuid();
+    this.placa = placa;
+    this.cor = cor;
+    this.marca = marca;
+  }
 }
+
+export default Automobile;
--- src/routes/automobile.routes.ts
+++ src/routes/automobile.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import Automobile from "../models/Automobile";
 
 const automobileRoutes = Router();
-
+const automobiles: Automobile[] = [];
 automobileRoutes.post("/", (request, response) => {
-  return response.json({ message: "Automobile Routes" });
+  const { placa, cor, marca } = request.body;
+
+  const automobile = new Automobile(placa, cor, marca);
+
+  automobiles.push(automobile);
+
+  return response.json(automobile);
 });
 
 export default automobileRoutes;
